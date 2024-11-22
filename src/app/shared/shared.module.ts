05a29a88de53e57defbcedfbf30a1fbe127237aa
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-22 10:55:17 -0600

    Nuevo dia, nuevas lineas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,13 +7,15 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     HomePageComponent,
     SidebarComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule,
@@ -21,7 +23,8 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
   ],
   exports: [
     HomePageComponent,
-    SidebarComponent
+    SidebarComponent,
+    SearchBoxComponent
   ]
 })
 export class SharedModule { }
