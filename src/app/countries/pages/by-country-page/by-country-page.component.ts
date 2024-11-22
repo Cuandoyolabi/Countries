b3d760d1d7e23ac86c72042a4f9801651b1037a6
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-22 16:45:52 -0600

    Se va agregando por region y por country

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Country } from '../../../shared/interfaces/country';
+import { CountriesService } from '../../../shared/services/countries.service';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
 })
 export class ByCountryPageComponent {
+
+  constructor( private CountriesService: CountriesService ){}
+
+  public countries: Country[] =[];
+
+  searchByCountry( term : string){
+    this.CountriesService.searchCapital( term )
+      .subscribe( countries => {
+        this.countries = countries })
+
+    }
 
 }
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../../shared/services/countries.service';
+import { Country } from '../../../shared/interfaces/country';
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html'
 })
 export class ByRegionPageComponent {
+
+  constructor( private CountriesService: CountriesService ){}
+
+  public countries: Country[] =[];
+
+  searchByRegion( term : string){
+    this.CountriesService.searchCapital( term )
+      .subscribe( countries => {
+        this.countries = countries })
+
+    }
 
 }
